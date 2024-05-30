0aacae926e5bf4033458a10392eba925b0e46839
Roger <100895716+rogergutierrez1719@users.noreply.github.com>
2024-05-30 00:27:20 -0600

    Se agrego el formulario

---
+++ Js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-aportacion').addEventListener('submit', function(event) {
+        event.preventDefault(); // Detener el envío del formulario por defecto
+
+        // Redirigir a la página de confirmación
+        window.location.href = './check.html';
+    });
+});
