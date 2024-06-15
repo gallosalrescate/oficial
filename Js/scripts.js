f061c956b686632b3e731bacbfdfb22ad60802fb
gallosalrescate <ronnyseverino874@gmail.com>
2024-06-15 08:38:46 -0600

    Agregamos Penultimos cambios

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('form-aportacion').addEventListener('submit', function(event) {
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('form-aportacion').addEventListener('submit', function (event) {
         event.preventDefault(); // Detener el envío del formulario por defecto
 
         // Redirigir a la página de confirmación
